1811a7c40c9fa1c631199e078d93cdf96ac7acad
Jason Lengstorf <jason@lengstorf.com>
2020-10-09 18:57:08 -0700

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
+import Layout from '../components/Layout';
 // import Img from 'gatsby-image'
-import Optiken from '../components/Optiken'
-import BlogRoll from '../components/BlogRoll'
-import Faq from '../components/Faq'
+import Optiken from '../components/Optiken';
+import BlogRoll from '../components/BlogRoll';
+import Faq from '../components/Faq';
 // import CustomLink from '../components/CustomLink'
 // import styled from 'styled-components'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import '../utils/fontawesome'
-import Fade from 'react-reveal/Fade'
+import '../utils/fontawesome';
+import Fade from 'react-reveal/Fade';
 // import logo from '../img/logo.png'
-import PulseText from 'react-pulse-text'
+import PulseText from 'react-pulse-text';
 // import PreviewCompatibleImageFluidBg from '../components/PreviewCompatibleImageFluidBg'
 // import PreviewCompatibleImageFluid from '../components/PreviewCompatibleImageFluid'
-import Bg from '../components/Bg'
-import BookImage from '../components/BookImage'
+import Bg from '../components/Bg';
+import BookImage from '../components/BookImage';
 
-export const HomePageTemplate = ({
-  home
-}) => {
+export const HomePageTemplate = ({ home }) => {
   return (
     <>
       {/*
@@ -36,7 +34,10 @@ export const HomePageTemplate = ({
         }}
       />
       */}
-      <div className='imageContainer' style={{ position: 'relative', height: '100vh' }}>
+      <div
+        className="imageContainer"
+        style={{ position: 'relative', height: '100vh' }}
+      >
         <Bg />
         {/*
       <Img
@@ -58,52 +59,92 @@ export const HomePageTemplate = ({
         */}
       </div>
 
-      <section className='hero is-medium' style={{ position: 'absolute', top: '12%', marginLeft: 'auto', right: '0', width: '100%' }}>
-        <div className='hero-body' style={{ paddingTop: '4rem', paddingBottom: '4rem' }}>
+      <section
+        className="hero is-medium"
+        style={{
+          position: 'absolute',
+          top: '12%',
+          marginLeft: 'auto',
+          right: '0',
+          width: '100%',
+        }}
+      >
+        <div
+          className="hero-body"
+          style={{ paddingTop: '4rem', paddingBottom: '4rem' }}
+        >
           <Fade>
-            <div className='container'>
-              <h1 className='title is-1 is-size-1-widescreen is-size-1-fullhd is-uppercase has-text-centered text-shadow is-spaced'>{home.mainpitch.title}
+            <div className="container">
+              <h1 className="title is-1 is-size-1-widescreen is-size-1-fullhd is-uppercase has-text-centered text-shadow is-spaced">
+                {home.mainpitch.title}
               </h1>
 
-              <PulseText text={home.mainpitch.subtitle} duration={6000} iterationCount={1}>
-                <h2 className='subtitle is-5 is-uppercase has-text-centered text-shadow has-text-white-ter'>{home.mainpitch.subtitle}
+              <PulseText
+                text={home.mainpitch.subtitle}
+                duration={6000}
+                iterationCount={1}
+              >
+                <h2 className="subtitle is-5 is-uppercase has-text-centered text-shadow has-text-white-ter">
+                  {home.mainpitch.subtitle}
                 </h2>
               </PulseText>
 
-
-
-              <div className='column has-text-centered' style={{ paddingTop: '4rem' }}>
-
-                <Link className='button is-medium is-outlined is-primary is-inverted' to='/#optiken'>
-                  mehr erfahren
+              <div
+                className="column has-text-centered"
+                style={{ paddingTop: '4rem' }}
+              >
+                <Link
+                  className="button is-medium is-outlined is-primary is-inverted"
+                  to="/#optiken"
+                >
+                  mehr erfahren test
                 </Link>
-
               </div>
             </div>
           </Fade>
         </div>
       </section>
 
-      <div className='column is-12 has-text-centered' id='optiken' style={{ position: 'relative', background: 'rgba(0,0,0,0.9)', paddingTop: '3rem' }}>
-        <h1 className='title is-2 is-uppercase'>Optiken</h1>
-        <div className='headline' />
+      <div
+        className="column is-12 has-text-centered"
+        id="optiken"
+        style={{
+          position: 'relative',
+          background: 'rgba(0,0,0,0.9)',
+          paddingTop: '3rem',
+        }}
+      >
+        <h1 className="title is-2 is-uppercase">Optiken</h1>
+        <div className="headline" />
       </div>
       <Optiken />
-      <div className='column is-12 has-text-centered' style={{ background: 'rgba(0,0,0,0.9)', position: 'relative' }}>
-        <Link className='button is-medium is-primary' to='/optiken'>
+      <div
+        className="column is-12 has-text-centered"
+        style={{ background: 'rgba(0,0,0,0.9)', position: 'relative' }}
+      >
+        <Link className="button is-medium is-primary" to="/optiken">
           alle Optiken erkunden
         </Link>
       </div>
-      <section id='info'>
-        <div className='hero is-medium has-text-centered'>
-          <div className='hero-body' style={{ position: 'relative' }}>
-            <div className='columns is-centered'>
-              <div className='column is-8' style={{ background: 'rgba(0,0,0,0.9)' }}>
-
-                <h1 className='title is-3 is-spaced text-shadow has-text-primary' style={{ position: 'relative' }}>
-                  Hier gibts die neueste Auflage von Military-Optics in gebundener, hochqualitativer Buchform
+      <section id="info">
+        <div className="hero is-medium has-text-centered">
+          <div className="hero-body" style={{ position: 'relative' }}>
+            <div className="columns is-centered">
+              <div
+                className="column is-8"
+                style={{ background: 'rgba(0,0,0,0.9)' }}
+              >
+                <h1
+                  className="title is-3 is-spaced text-shadow has-text-primary"
+                  style={{ position: 'relative' }}
+                >
+                  Hier gibts die neueste Auflage von Military-Optics in
+                  gebundener, hochqualitativer Buchform
                 </h1>
-                <h2 className='subtitle text-shadow has-text-white-ter' style={{ position: 'relative' }}>
+                <h2
+                  className="subtitle text-shadow has-text-white-ter"
+                  style={{ position: 'relative' }}
+                >
                   Schreiben Sie mir einfach und ich melde mich umgehend.
                 </h2>
 
@@ -121,8 +162,8 @@ export const HomePageTemplate = ({
                 */}
               </div>
             </div>
-            <div className='column is-12'>
-              <Link to='/kontakt' className='button is-medium is-primary'>
+            <div className="column is-12">
+              <Link to="/kontakt" className="button is-medium is-primary">
                 Anfrage senden
               </Link>
             </div>
@@ -130,41 +171,44 @@ export const HomePageTemplate = ({
         </div>
       </section>
       <BlogRoll />
-      <div className='column is-12 has-text-centered' style={{ background: 'rgba(0,0,0,0.9)', position: 'relative' }}>
-        <Link className='button is-medium is-primary' to='/blog'>
+      <div
+        className="column is-12 has-text-centered"
+        style={{ background: 'rgba(0,0,0,0.9)', position: 'relative' }}
+      >
+        <Link className="button is-medium is-primary" to="/blog">
           alle Einträge
         </Link>
       </div>
       <Faq />
     </>
-  )
-}
+  );
+};
 
 class HomePage extends React.Component {
-  render () {
-    const { data } = this.props
+  render() {
+    const { data } = this.props;
     const {
-      data: { footerData }
-    } = this.props
-    const { frontmatter: home } = data.homePageData.edges[0].node
+      data: { footerData },
+    } = this.props;
+    const { frontmatter: home } = data.homePageData.edges[0].node;
 
     return (
       <Layout footerData={footerData}>
         <HomePageTemplate home={home} />
       </Layout>
-    )
+    );
   }
 }
 
 HomePage.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object
-    })
-  })
-}
+      frontmatter: PropTypes.object,
+    }),
+  }),
+};
 
-export default HomePage
+export default HomePage;
 
 export const pageQuery = graphql`
   query HomePageQuery {
@@ -181,7 +225,9 @@ export const pageQuery = graphql`
       }
     }
     ...LayoutFragment
-    homePageData: allMarkdownRemark(filter: { frontmatter: { templateKey: { eq: "home-page" } } }) {
+    homePageData: allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "home-page" } } }
+    ) {
       edges {
         node {
           frontmatter {
@@ -194,4 +240,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
