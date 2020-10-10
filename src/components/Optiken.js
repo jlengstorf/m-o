15f4ed29804f4c5608d0b64384a0f0d8485e5dff
Jason Lengstorf <jason@lengstorf.com>
2020-10-10 15:16:18 -0700

    wip: try without webp

--- src/components/Optiken.js
+++ src/components/Optiken.js
@@ -1,26 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import Img from 'gatsby-image'
-import Fade from 'react-reveal/Fade'
-import PreviewCompatibleImageFluid from '../components/PreviewCompatibleImageFluid'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Link, graphql, StaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+import Fade from 'react-reveal/Fade';
+import PreviewCompatibleImageFluid from '../components/PreviewCompatibleImageFluid';
 
 const Optik = styled.article`
   &:hover {
-    transform: perspective(1200px) translateZ(0px) translateX(0px) translateY(0px) rotateY(-10deg);
+    transform: perspective(1200px) translateZ(0px) translateX(0px)
+      translateY(0px) rotateY(-10deg);
     transition: 0.7s ease;
-    box-shadow: 200px 200px 200px rgba(255,244,255,1);
+    box-shadow: 200px 200px 200px rgba(255, 244, 255, 1);
     // transform: scale(1.02);
     // transition: all 0.3s ease;
     .hoverLine {
       // background: linear-gradient(to right, #FF0000 0%, #F30C00 100%) !important;
-      background: linear-gradient(to right, #347738 10%, #A39976 30%, #347738 50%, #A39976 70%, #347738 90% ) !important;
+      background: linear-gradient(
+        to right,
+        #347738 10%,
+        #a39976 30%,
+        #347738 50%,
+        #a39976 70%,
+        #347738 90%
+      ) !important;
       height: 4px;
       width: 110%;
       opacity: 1;
       transition: width 1s;
-      transition-timing-function: cubic-bezier(.455,.03,.515,.955);
+      transition-timing-function: cubic-bezier(0.455, 0.03, 0.515, 0.955);
     }
   }
   .headline:after {
@@ -30,10 +38,10 @@ const Optik = styled.article`
     left: 50%;
     height: 4px;
     bottom: -15px;
-    background: linear-gradient(to right, #FF0000 0%, #F30C00 100%);
-    box-shadow: 0 0 5px rgba(0,0,0,.5);
+    background: linear-gradient(to right, #ff0000 0%, #f30c00 100%);
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
     border-radius: 3px;
-    content: "";
+    content: '';
     display: block;
   }
   .hoverLine {
@@ -53,7 +61,7 @@ const Optik = styled.article`
     transition: all 0.3s linear;
   }
 
-  .img-container{
+  .img-container {
     // min-height: 200px;
     // max-height: 170px;
     // background: #fff;
@@ -73,72 +81,82 @@ const Optik = styled.article`
   .img-container:hover .room-link {
     transform: translate(-50%, -50%) scale(1);
   }
-`
+`;
 
 class Optiken extends React.Component {
-  render () {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+  render() {
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
     return (
-      <section id='optiken' className='section' style={{ background: 'rgba(0,0,0,0.9)', position: 'relative' }}>
-        <div className='container'>
-          <div className='columns is-multiline is-centered'>
+      <section
+        id="optiken"
+        className="section"
+        style={{ background: 'rgba(0,0,0,0.9)', position: 'relative' }}
+      >
+        <div className="container">
+          <div className="columns is-multiline is-centered">
+            {posts &&
+              posts
+                .filter(({ node: post }) => post.frontmatter.featured === 'Ja')
+                .map(({ node: post }) => (
+                  <div className="is-parent column is-4" key={post.id}>
+                    <Fade>
+                      <Optik
+                        style={{
+                          padding: '0',
+                          maxWidth: '350px',
+                          margin: '0 auto',
+                        }}
+                      >
+                        <Link to={post.fields.slug}>
+                          <div style={{ position: 'relative' }}>
+                            <div className="img-container">
+                              <div
+                                className="title is-size-4 has-text-centered is-uppercase"
+                                style={{
+                                  position: 'absolute',
+                                  zIndex: '3000',
+                                  width: '100%',
+                                  background: 'rgba(0,0,0,.8)',
+                                  padding: '15px',
+                                  overflow: 'hidden',
+                                }}
+                              >
+                                {post.frontmatter.title}
+                                <div className="hoverLine" />
+                              </div>
 
-            {posts && (posts
-              .filter(({ node: post }) => (post.frontmatter.featured === 'Ja')).map(({ node: post }) => (
-                <div
-                  className='is-parent column is-4'
-                  key={post.id}
-                >
-                  <Fade>
-                    <Optik
-                      style={{ padding: '0', maxWidth: '350px', margin: '0 auto' }}
-                    >
-                      <Link to={post.fields.slug}>
-                        <div style={{ position: 'relative' }}>
-                          <div className='img-container'>
-                            <div
-                              className='title is-size-4 has-text-centered is-uppercase'
-                              style={{ position: 'absolute', zIndex: '3000', width: '100%', background: 'rgba(0,0,0,.8)', padding: '15px', overflow: 'hidden' }}
-                            >
-                              {post.frontmatter.title}
-                              <div className='hoverLine' />
-                            </div>
+                              <div className="button is-medium is-primary room-link">
+                                Details
+                              </div>
 
-                            <div
-                              className='button is-medium is-primary room-link'
-                            >
-                              Details
-                            </div>
+                              <Img
+                                fluid={
+                                  post.frontmatter.image.childImageSharp.fluid
+                                }
+                                alt={post.frontmatter.title}
+                                style={{ width: 'auto' }}
+                                className="image"
+                              />
 
-
-                            <Img
-                              fluid={post.frontmatter.image.childImageSharp.fluid}
-                              alt={post.frontmatter.title}
-                              style={{ width: 'auto' }}
-                              className='image'
-                            />
-
-
-                            {/*
+                              {/*
                             <PreviewCompatibleImageFluid imageInfo={{
                               image: post.frontmatter.image,
                               alt: post.frontmatter.title + ' ' + 'Bild'
                             }}
                             />
                             */}
+                            </div>
                           </div>
-                        </div>
-                      </Link>
-                    </Optik>
-                  </Fade>
-                </div>
-              )))}
+                        </Link>
+                      </Optik>
+                    </Fade>
+                  </div>
+                ))}
           </div>
         </div>
       </section>
-
-    )
+    );
   }
 }
 
@@ -148,43 +166,45 @@ Optiken.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
 export default () => (
   <StaticQuery
     query={graphql`
-    query OptikenQuery {
-      allMarkdownRemark(
-
-        sort: { order: DESC, fields: [frontmatter___date] },
-        filter: { frontmatter: { templateKey: { eq: "optiken-post" } }}
-      ) {
-        edges {
-          node {
-            id
-            fields {
-              slug
-            }
-            frontmatter {
-              image {
-                childImageSharp {
-                  fluid(maxWidth: 200, maxHeight: 170, quality: 92, srcSetBreakpoints: [350, 700, 1050, 1400]) {
-                    ...GatsbyImageSharpFluid_withWebp
-                    presentationWidth
+      query OptikenQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "optiken-post" } } }
+        ) {
+          edges {
+            node {
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                image {
+                  childImageSharp {
+                    fluid(
+                      maxWidth: 200
+                      maxHeight: 170
+                      quality: 92
+                      srcSetBreakpoints: [350, 700, 1050, 1400]
+                    ) {
+                      ...GatsbyImageSharpFluid
+                      presentationWidth
+                    }
                   }
                 }
+                title
+                date(formatString: "DD.MM.YYYY")
+                featured
               }
-              title
-              date(formatString: "DD.MM.YYYY")
-              featured
             }
           }
         }
       }
-    }
     `}
-    render={(data, count) => (
-      <Optiken data={data} count={count} />
-    )}
+    render={(data, count) => <Optiken data={data} count={count} />}
   />
-)
+);
